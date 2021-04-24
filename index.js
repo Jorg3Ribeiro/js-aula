5bc891defff508dd2a42ed67fae20e631f996e2c
Jorg3Ribeiro <jorge_ribeiro@live.com>
2021-04-24 10:36:45 -0300

    3 files modified, 1 added, 43 deleted

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Import stylesheets
-import './css/style.css';
-import './css/bootstrap.css'
-import './js/bootstrap.js'
+import './style.css';
 
 // Write Javascript code!
+
+function cadastrar(){
+  var tabela = document.getElementById('tabela_cadastro_aula')
+  var formularioAula = document.getElementById('formulario_cadastro_aula')
+
+  tabela.hide
+  formularioAula.show
+}
