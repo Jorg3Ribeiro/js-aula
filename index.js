6a2f4c5ccece46199bba66cc6b4a64b09b53bac5
Jorg3Ribeiro <jorge_ribeiro@live.com>
2021-04-24 11:11:20 -0300

    2 files modified

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 // Import stylesheets
-import './css/style.css';
-import './css/bootstrap.css';
-import './js/bootstrap.js';
+import "./css/style.css";
+import "./css/bootstrap.css";
+import "./js/bootstrap.js";
 
 // Write Javascript code!
 (function(w) {
+  var tabela = document.getElementById("tabela_cadastro_aula");
+  var formularioAula = document.getElementById("formulario_cadastro_aula");
+  var formularioDisciplina = document.getElementById(
+    "formulario_cadastro_disciplina"
+  );
+  var formularioProfessor = document.getElementById(
+    "formulario_cadastro_professor"
+  );
 
-var cadastrar = function(){
-  var tabela = document.getElementById('tabela_cadastro_aula');
-  var formularioAula = document.getElementById('formulario_cadastro_aula');
+  var cadastrar = function() {
+    tabela.style.display = "none";
+    formularioAula.style.display = "block";
+  };
 
-  tabela.style.display = "none";
-  formularioAula.style.display = "block";
+  var voltarTabela = function() {
+    formularioAula.style.display = "none";
+    tabela.style.display = "block";
+  };
 
-}
+  var cadastrarDisciplina = function() {
+    formularioAula.style.display = "none";
+    formularioDisciplina.style.display = "block";
+  };
 
-w.cadastrar = cadastrar;
+  var voltarFormulario = function() {
+    formularioDisciplina.style.display = "none";
+    formularioProfessor.style.display = "none";
+    formularioAula.style.display = "block";
+  };
 
+  var cadastrarProfessor = function() {
+    formularioAula.style.display = "none";
+    formularioProfessor.style.display = "block";
+  };
+
+  (w.cadastrar = cadastrar),
+    (w.voltarTabela = voltarTabela),
+    (w.cadastrarDisciplina = cadastrarDisciplina),
+    (w.voltarFormulario = voltarFormulario),
+    (w.cadastrarProfessor = cadastrarProfessor);
 })(window);
